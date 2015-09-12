ecdcdc40d55d969bb232a02829241c86cb9215c5
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-09-12 11:43:04 +0300

    Created outlets for labels.

---
+++ WeatherTracker/WeatherViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherTracker
+//
+//  Created by Alexey Tsymlov on 9/12/15.
+//  Copyright (c) 2015 Specialist. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var forecastLabel0: UILabel!
+    @IBOutlet weak var forecastLabel1: UILabel!
+    @IBOutlet weak var forecastLabel2: UILabel!
+    @IBOutlet weak var forecastLabel3: UILabel!
+}
+
